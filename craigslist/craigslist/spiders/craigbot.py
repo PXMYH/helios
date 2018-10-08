816f029993724685f826a55aa874cd2a153d15a6
Michael Hu <pxmyhdev@gmail.com>
2018-10-08 14:14:23 -0400

    traverse through multiple pages

--- craigslist/craigslist/spiders/craigbot.py
+++ craigslist/craigslist/spiders/craigbot.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from scrapy import Request
 
 class CraigbotSpider(scrapy.Spider):
     name = 'craigbot'
     allowed_domains = ['craigslist.ca']
+    # TODO: accept search URL from command line
+    # tweak start_urls to get to the result faster
     # start_urls = ['https://vancouver.craigslist.ca/search/apa']
     start_urls = ['https://vancouver.craigslist.ca/search/van/apa?min_bedrooms=2&max_bedrooms=2']
 
@@ -13,6 +15,11 @@ class CraigbotSpider(scrapy.Spider):
         listings = response.xpath('//p[@class="result-info"]')
         print ("\n*************\nlistings: {0}".format(listings))
         for listing in listings:
+            # get relative url and construct absolute url
+            # relative_url = listing.xpath('a/@href').extract_first()
+            # print ("relative url {0}".format(relative_url))
+            # absolute_url = response.urljoin(relative_url)
+
             title = listing.xpath('a[@class="result-title hdrlnk"]/text()').extract_first()
             price = listing.xpath('span[@class="result-meta"]/span[@class="result-price"]/text()').extract()
             neighbourhood = listing.xpath('span[@class="result-meta"]/span[@class="result-hood"]/text()').extract()
@@ -32,3 +39,8 @@ class CraigbotSpider(scrapy.Spider):
                     space = "0ft"
             
             yield {'Title': title, 'Hood': neighbourhood, 'Price': price, 'Area': space}
+
+        relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
+        absolute_next_url = response.urljoin(relative_next_url)
+        
+        yield Request(absolute_next_url, callback=self.parse)
