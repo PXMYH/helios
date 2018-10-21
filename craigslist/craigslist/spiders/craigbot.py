b6377cc1bd3e7bdec8225c1e6edd7b7d22d32413
Michael Hu <pxmyhdev@gmail.com>
2018-10-20 23:33:29 -0400

    traverse listing pages

--- craigslist/craigslist/spiders/craigbot.py
+++ craigslist/craigslist/spiders/craigbot.py
@@ -5,6 +5,7 @@ from scrapy import Request
 class CraigbotSpider(scrapy.Spider):
     name = 'craigbot'
     allowed_domains = ['craigslist.ca']
+
     # TODO: accept search URL from command line
     # tweak start_urls to get to the result faster
     # start_urls = ['https://vancouver.craigslist.ca/search/apa']
@@ -16,9 +17,9 @@ class CraigbotSpider(scrapy.Spider):
         print ("\n*************\nlistings: {0}".format(listings))
         for listing in listings:
             # get relative url and construct absolute url
-            relative_url = listing.xpath('a/@href').extract_first()
-            # print ("relative url {0}".format(relative_url))
-            absolute_url = response.urljoin(relative_url)
+            relative_specific_list_url = listing.xpath('a/@href').extract_first()
+            # print ("relative url {0}".format(relative_specific_list_url))
+            absolute_specific_list_url = response.urljoin(relative_specific_list_url)
 
             title = listing.xpath('a[@class="result-title hdrlnk"]/text()').extract_first()
             price = listing.xpath('span[@class="result-meta"]/span[@class="result-price"]/text()').extract()
@@ -46,12 +47,18 @@ class CraigbotSpider(scrapy.Spider):
 
             yield {'Title': title, 'Hood': neighbourhood, 'Price': price, 'Area': space, 'UnitPrice': unit_price}
 
+            # parse each real estate listing page
+            yield Request(absolute_specific_list_url, callback=self.parse_page, meta={'URL': absolute_specific_list_url})
+
         relative_next_url = response.xpath('//a[@class="button next"]/@href').extract_first()
-        # absolute_next_url = response.urljoin(relative_next_url)
-        
-        yield Request(absolute_url, callback=self.parse_page, meta={'URL': absolute_url})
+        absolute_next_url = response.urljoin(relative_next_url)
+
+        # move to the next listing summary page
+        yield Request(absolute_next_url, callback=self.parse)
 
     def parse_page(self, response):
+        ''' parsing each page to extract detailed information'''
+        # fetch relevant detailed information such as ad description, bed/bath number, availability etc.
         description = "".join(line for line in response.xpath('//*[@id="postingbody"]/text()').extract())
         posted_timestamp = response.xpath('//p[@class="postinginfo reveal"]/time[@class="date timeago"]/text()').extract()
         bed_bath = "".join(" " + line for line in response.xpath('//p[@class="attrgroup"]/span[@class="shared-line-bubble"]/b/text()').extract())
@@ -69,4 +76,3 @@ class CraigbotSpider(scrapy.Spider):
         response.meta['other_attr'] = other_attr
 
         yield response.meta
-
