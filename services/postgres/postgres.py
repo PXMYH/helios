afdfe7e41ea91196de02c8faadc71c516e8c5acc
Michael Hu <pxmyhdev@gmail.com>
2019-01-27 18:30:04 -0500

    implement CRUD operation to interact with postgres database

--- services/postgres/postgres.py
+++ services/postgres/postgres.py
@@ -1,50 +1,105 @@
 import os
 import sys
 import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, BigInteger, SmallInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_info = "postgres://postgres@localhost:5432"
+db_info = "postgres://postgres@localhost:5432/helios"
 db = create_engine(db_info)
 base = declarative_base()
-
-class Rental(base):
-    __tablename__ = 'craigslist_real_estate'
-
-    id = Column(Integer, primary_key=True)
-    location = Column(String(255))
-    bedroom = Column(Integer, autoincrement=False)
-    bathroom = Column(Integer, autoincrement=False)
-    den = Column(Integer, autoincrement=False)
-    price = Column(Integer)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
-
-
+# establish session
 Session = sessionmaker(db)
 session = Session()
 
-base.metadata.create_all(db)
-# Create 
-vancouver_rental = Rental(location="Vancouver", bedroom="2", bathroom="2", den="0", price="600000", updated_at="1000302")  
-session.add(vancouver_rental)  
-session.commit()
+
+class Rental_Database(base):
+
+    __tablename__ = 'test_table'
+
+    id = Column(BigInteger, primary_key=True)
+    location = Column(String(255))
+    bedroom = Column(SmallInteger, autoincrement=False)
+    bathroom = Column(SmallInteger, autoincrement=False)
+    den = Column(SmallInteger, autoincrement=False)
+    price = Column(Integer)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    # create all tables defined
+    base.metadata.create_all(db)
+
+    def __init__(self, location, bedroom, bathroom, den, price, updated_at):
+        ''' constructor '''
+        self.location = location
+        self.bedroom = bedroom
+        self.bathroom = bathroom
+        self.den = den
+        self.price = price
+        self.updated_at = updated_at
+
+
+class Rental(Rental_Database):
+    def __init__(self, location, bedroom, bathroom, den, price, updated_at):
+        print "Initializing rental class ..."
+        self.location = location
+        self.bedroom = bedroom
+        self.bathroom = bathroom
+        self.den = den
+        self.price = price
+        self.updated_at = updated_at
+        self.rental_database_record = Rental_Database(
+            self.location, self.bedroom, self.bathroom, self.den, self.price, self.updated_at)
+
+    def save_record(self):
+        session.add(self.rental_database_record)
+        session.commit()
+        print("Record is saved successfully")
+
+    def get_records(self):
+        print("Getting records ...")
+        rental_records = session.query(self.__class__)
+        for rental_record in rental_records:
+            print(rental_record.location)
+
+    def update_record(self, key, value):
+        print(
+            "Updating record attribute: {0} with new value {1}".format(key, value))
+        # map key to attributes
+        # TODO: this is not efficient implementation, first draft
+        if key == "location":
+            self.rental_database_record.location = value
+            session.commit()
+        elif key == "bedroom":
+            self.rental_database_record.bedroom = value
+        elif key == "bathroom":
+            self.rental_database_record.bathroom = value
+        elif key == "den":
+            self.rental_database_record.den = value
+        elif key == "price":
+            self.rental_database_record.price = value
+        else:
+            print("No attribute {0} found! No update operation takes place")
+            return
+        session.commit()
+
+    def delete_record(self):
+        print("Deleting record ...")
+        session.delete(self.rental_database_record)
+        session.commit()
+
+
+# CRUD operations
+# Create
+rental_instance = Rental("Vancouver", "2", "2", "0", "600000", "1000302")
+rental_instance.save_record()
 
 # Read
-rentals = session.query(Rental)  
-for rental in rentals:  
-    print(rental.location)
+rental_instance.get_records()
 
 # Update
-vancouver_rental.location = "Toronto"  
-session.commit()
+rental_instance.update_record("location", "Toronto")
 
 # Delete
-session.delete(vancouver_rental)  
-session.commit()
-
-
-# if __name__ == "__main__":
-#     print ("Initiating Postgres DB connection ...")
+rental_instance.delete_record()
