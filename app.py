efcdb944cec32a27699bc19af9832924812549ed
Michael Hu <pxmyhdev@gmail.com>
2019-03-02 11:13:40 -0500

    start up scrapying at flask app startup #22

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from flask import jsonify
 import connexion
 from services.bots import Bots
 from celery import Celery
+import requests
+import time
+import threading
 
 # Create application instance
 # app = connexion.FlaskApp(__name__, specification_dir="swagger/")
@@ -11,12 +14,31 @@ from celery import Celery
 # Read the swagger.yml file to configure the endpoints
 # app.add_api('api.yaml')
 
-app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+def make_celery(app):
+    celery = Celery(
+        app.import_name,
+        backend=app.config['CELERY_RESULT_BACKEND'],
+        broker=app.config['CELERY_BROKER_URL']
+    )
+    celery.conf.update(app.config)
+
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
+    return celery
+
+
+app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+celery = make_celery(app)
 
 
 @app.route("/")
@@ -24,18 +46,44 @@ def index():
     return jsonify("Welcome to Helios System")
 
 
-@celery.task
+@celery.task()
+@app.before_first_request
 @app.route("/v1/start")
-def reap():
-    """ 
-    start bot crawling sequence 
-    """
+def activate_reap():
 
-    print ("Initiating Bot Sequence ...")
+    def reap():
+        """
+        start bot crawling sequence
+        """
 
-    test_bot = Bots()
-    test_bot.run_craigbot()
-    return jsonify(interval="10m", result="SUCCESS")
+        print ("Initiating Bot Sequence ...")
+
+        test_bot = Bots()
+        test_bot.run_craigbot()
+        return jsonify(interval="10m", result="SUCCESS")
+
+    thread = threading.Thread(target=reap)
+    thread.start()
+
+
+def start_runner():
+    def start_loop():
+        not_started = True
+        while not_started:
+            print('In start loop')
+            try:
+                r = requests.get('http://localhost:5000/')
+                if r.status_code == 200:
+                    print('Server started, quiting start_loop')
+                    not_started = False
+                print(r.status_code)
+            except:
+                print('Server not yet started')
+            time.sleep(2)
+
+    print('Started runner')
+    thread = threading.Thread(target=start_loop)
+    thread.start()
 
 
 if __name__ == '__main__':
@@ -44,4 +92,6 @@ if __name__ == '__main__':
 
     # app.add_api('api.yaml', resolver=RestyResolver('api'))
     # app.add_api('api.yml')
+    print('Starting runner')
+    start_runner()
     app.run(debug=True)
